8d386054ecfc51ef16c4b48cc65ffee8cf83defb
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2024-02-01 21:55:41 +0600

    first commit

--- arrow.js
+++ arrow.js
@@ -1,15 +1,20 @@
-function add(a,b){
-    const result = a+b;
-    return result;
-}
-console.log(add(2,3));
-function show(){
-    console.log('Hellow World');
-}
-show()
+// function add(a,b){
+//     const result = a+b;
+//     return result;
+// }
+// console.log(add(2,3));
+// function show(){
+//     console.log('Hellow World');
+// }
+// show()
 
-function printNumber(x){
-    console.log('Value  is', x);
-}
+// function printNumber(x){
+//     console.log('Value  is', x);
+// }
 
-printNumber(50)
+// printNumber(50)
+function add(x,y){
+const result = x+y;
+return result;
+}
+console.log(add(3,2));
