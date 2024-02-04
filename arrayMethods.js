d7c91539f9ea526badbbdf9ae8424df66f5945f1
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2024-02-04 21:37:21 +0600

    map

---
+++ arrayMethods.js
@@ -0,0 +1,29 @@
+// forEach, Map, Find, Filter, Reduce
+
+// const items = [1,2,6,4,5]
+// // for(let item of items){
+// //     console.log(item);
+// // }
+
+// for(let i = 0; i<items.length; i++){
+//     console.log(items[i]);
+// }
+// const arr = [1,2,3];
+// const a = (x) => console.log(x);
+// arr.forEach(a)
+// arr.forEach((x) => console.log(x))
+
+// const fruits = ['mango, banana, orange'];
+
+// fruits.forEach(item =>{
+//     console.log(item);
+// })
+
+// let sum = 0;
+// arr.forEach( item =>{
+//     sum += item
+// })
+// console.log(sum);
+
+const arr = [11,22,33,44,55];
+arr.map(item => console.log(item))
--- default.js
+++ default.js
@@ -1,2 +1,4 @@
 const add = (a=5,b=5) => a+b;
 console.log(add(6));
+const sum = (x=10,y=20) => x+y;
+console.log(sum(50,50));
