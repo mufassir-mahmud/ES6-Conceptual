e5843b8c56a3482fdf9026c094cc0a680faf2095
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2024-02-05 20:28:42 +0600

    filter

--- arrayMethods.js
+++ arrayMethods.js
@@ -25,16 +25,65 @@
 // })
 // console.log(sum);
 
-const arr = [11,22,33,44,55];
-arr.map(item => console.log(item))
+// const arr = [11,22,33,44,55];
+// arr.map(item => console.log(item))
 
 // const result = arr.forEach((item) => item);
 // console.log(result);
-const result = arr.map((item) => item*2);
-console.log(result);
+// const result = arr.map((item) => item*2);
+// console.log(result);
 
-const fruits = ['mango','bango', 'cango', 'tango'];
-fruits.forEach((item) => console.log(item))
-fruits.forEach((item, index, arr) => console.log(item, index,arr))
+// const fruits = ['mango','bango', 'cango', 'tango'];
+// fruits.forEach((item) => console.log(item))
+// fruits.forEach((item, index, arr) => console.log(item, index,arr))
 
 // forEach = element, index number, main array
+const fruits = ['mango','bango', 'cango', 'tango'];
+
+// const find = (fruits) =>{
+//     for(let fruit of fruits){
+//         if(fruit === 'cango'){
+//             return fruit
+//         }
+        
+//     }
+//     return null;
+// }
+// console.log(find(fruits));
+
+// const y = fruits.find((item) => item === 'cango')
+// console.log(y);
+
+// const fruit = fruits.find(item => item === 'cango')
+// console.log(fruit);
+
+
+// const numbers = [11,22,33,44,55,66];
+
+// const filter = numbers =>{
+//     const result = [];
+//     for(let number of numbers){
+//         if(number % 2 === 0){
+//             result.push(number)
+//         }
+//     }
+//     return result
+// }
+
+// console.log(filter(numbers));
+
+const numbers = [11,33,55,77];
+const filter = numbers =>{
+    const result = [];
+    for( let number of numbers){
+        if(number % 2 !== 0){
+            result.push(number)
+        }
+    }
+    return result
+}
+const x = filter(numbers);
+console.log(x);
+
+const y = numbers.filter((number) => number%2 !== 0);
+console.log(y);
