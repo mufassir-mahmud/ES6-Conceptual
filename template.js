d93e941792300dd79cd2ba6bfe7624baf2f123ae
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2024-02-06 11:40:17 +0600

    template

---
+++ template.js
@@ -0,0 +1,16 @@
+const myName ='Mufassir Mahmud';
+const age = 22;
+const address = 'Feni';
+const bowerName = 'Katappar Beti';
+const sosurBari = 'Rampur';
+const numbers = [11,12,20]
+const sentense = `Amar name ${myName}
+amr boyos ${age}
+amr bari ${address}
+amr bow er name ${bowerName}
+amr sosur bari ${sosurBari}
+amr Special ${numbers.map(number => number)}
+
+
+`;
+console.log(sentense);
