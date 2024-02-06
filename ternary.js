8d87cf0f84c4e9355c4445c9b382ade9975dd739
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2024-02-06 20:26:29 +0600

    ternary.js

---
+++ ternary.js
@@ -0,0 +1,13 @@
+// const x = 5;
+// // if(x> 5){
+// //     console.log('x is greater than 5');
+// // }
+// // else{
+// //     console.log('x is less than 5');
+// // }
+
+// x> 5 ? console.log('x is greater than 5') : console.log('x is less than 5');
+
+const age = 22;
+
+age>18 ? console.log('you are adult') : console.log('You are child');
