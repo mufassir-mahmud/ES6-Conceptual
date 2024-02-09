26a9fd09fce09c41bc5a37a43e6c324716acda05
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2024-02-09 11:37:30 +0600

    objectAccess.js

--- objectAccess.js
+++ objectAccess.js
@@ -1,8 +1,48 @@
-const person = {
-    name: 'Mufassir',
-    age: 22,
-    class: 'Honourse',
+// const person = {
+//     name: 'Mufassir',
+//     age: 22,
+//     class: 'Honourse',
+// }
+
+// const property = 'age';
+// console.log(person[property]);
+const complexObject = {
+    id: 1,
+    name: 'Main Object',
+    info : {
+        description : 'This is a complex nested object',
+        author: {
+            firstName: 'John',
+            lastName: 'Doe'
+        }
+    },
+    data: {
+        values: [11,22,33],
+        settings: {
+            isEnabled: true,
+            maxItems: 5
+        }
+    }
 }
 
-const property = 'age';
-console.log(person[property]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
