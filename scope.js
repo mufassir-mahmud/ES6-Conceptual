40f1896d9f313e87828aba18710e8f4acc0647fd
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2024-02-10 12:31:28 +0600

    console.log(x);

---
+++ scope.js
@@ -0,0 +1,29 @@
+// const x = 5;
+
+// {
+//     console.log(x);
+// }
+
+// {
+
+//     let x = 5;
+//     x++;
+    
+// }
+// console.log(x);
+
+// {
+//     var x = 5;
+//     x++;
+//     console.log(x);
+// }
+
+
+// console.log(x);
+
+const show = () =>{
+    var x = 5;
+    console.log(x);
+}
+show()
+console.log(x);
