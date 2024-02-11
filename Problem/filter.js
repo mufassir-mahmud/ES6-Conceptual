03acdf09f6c8e0babc2a78a7bf28aa8ecc775cd1
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2024-02-11 17:13:07 +0600

    filter.js

---
+++ Problem/filter.js
@@ -0,0 +1,15 @@
+const students = [
+    {name: 'Mufassir Mahmud' , email: 'mufassir389', avg: 56, fiftyPercent: true},
+    {name: 'Mehedi Hasan' , email: 'mehedi#56', avg: 57, fiftyPercent: true},
+    {name: 'Omar Farouk' , email: 'omar389', avg: 47, fiftyPercent: false},
+    {name: 'ABM Fahum' , email: 'fahim389', avg: 56, fiftyPercent: true},
+    {name: 'Nomayren Ohin' , email: 'ohin69', avg: 40, fiftyPercent: false},
+    {name: 'Kazi Sakib' , email: 'sakib5678', avg: 46, fiftyPercent: false},
+
+]
+
+// const scic = students.filter((students) => students.avg >= 50 && students.fiftyPercent == true);
+// console.log(scic);
+
+const scic = students.filter((students) => students.avg >= 50 && students.fiftyPercent == true);
+console.log(scic);
